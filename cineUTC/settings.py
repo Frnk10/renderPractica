09e86ebbd12269619ccd94058ddf8b1edb87c8d7
xavierp10 <franciscoporras1000@gmail.com>
2024-07-27 17:28:09 -0500

    Base de datos

--- cineUTC/settings.py
+++ cineUTC/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'cineUTC.wsgi.application'
 
 # POSTGRESQL
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(default='postgres://user:password@localhost:5432/mysite')
 }
 
 # Password validation
