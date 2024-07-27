4dc06d45c5fe78cdb25984f7f2ea8a8457dd91f1
xavierp10 <franciscoporras1000@gmail.com>
2024-07-27 16:17:37 -0500

    Nuevo

--- cineUTC/settings.py
+++ cineUTC/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'cineUTC.wsgi.application'
 # POSTGRESQL
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/postgres',
+        default='postgresql://postgres:postgres@localhost:5432/bddCineUTC',
         conn_max_age=600    
     )
 }
